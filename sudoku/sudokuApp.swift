2165f7df4bcd3dea5ad809bc154f9d330d76249a
tanat <31177223+poroye@users.noreply.github.com>
2021-09-15 00:21:14 -0700

    Initial Commit

---
+++ sudoku/sudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sudokuApp.swift
+//  sudoku
+//
+//  Created by ธนัท แสงเพิ่ม on 15/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct sudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
