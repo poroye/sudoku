0b67087df777bb163a840909f1994e1f18a06c92
tanat <31177223+poroye@users.noreply.github.com>
2021-09-18 20:18:38 -0700

    update
    add fill number onstart and show on swiftUI

--- sudoku/ContentView.swift
+++ sudoku/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var grid = Grid()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            ForEach(grid.grid.indices,id:\.self){row in
+                HStack{
+                    ForEach(grid.grid[row].indices,id:\.self){col in
+                        Text("\(grid.historyGrid[grid.now][row][col])")
+                            .font(.body)
+                            .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .padding(10)
+                            .background(Color.blue)
+                    }
+                }
+            }
+        }.onAppear(perform: {
+            grid.fillGrid(grid: &grid.grid)
+            grid.historyGrid.append(grid.grid)
+            grid.now += 1
+        })
     }
 }
 
